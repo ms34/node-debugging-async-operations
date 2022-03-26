18e587fbf0de72f949e263e477f6b7eaacd872cf
ms34 <onethreems@gmail.com>
2022-03-26 10:39:23 +0300

    uploading debugging-aync-operations project

---
+++ example-4.js
@@ -0,0 +1,29 @@
+//
+// Run me with --expose-gc
+// 
+// node --expose-gc example.js
+//
+
+const { AsyncDebugger } = require("./lib/async-debugger");
+
+console.log("Starting up!"); 
+
+function doTimeout() {
+    console.log("Starting timeout.");
+
+    setTimeout(() => {
+        
+        setTimeout(() => {
+            console.log("Timeout finished.");
+        }, 2000);
+    
+    }, 2000);
+}
+
+const asyncDebugger = new AsyncDebugger();
+asyncDebugger.notifyComplete(() => {
+    asyncDebugger.debug("test-1");
+});
+asyncDebugger.startTracking("test-1", doTimeout);
+
+console.log("End of script");
